d22b53fc224714fa7ed04d68978433ee0f483e95
IljaKroonen <ilja.kroonen@gmail.com>
2014-02-23 13:42:08 +0100

    Added forgotten file

---
+++ src/org/black_mesa/webots_remote_control/remote_object_state/RemoteCameraState.java
@@ -0,0 +1,57 @@
+package org.black_mesa.webots_remote_control.remote_object_state;
+
+import java.io.Serializable;
+
+/**
+ * @author Ilja Kroonen
+ */
+public class RemoteCameraState implements Cloneable, Serializable {
+	private static final long serialVersionUID = -2000084337375288247L;
+	private double positionX, positionY, positionZ;
+	private double orientationX, orientationY, orientationZ, orientationAngle;
+
+	public RemoteCameraState(double positionX, double positionY, double positionZ, double orientationX, double orientationY,
+			double orientationZ, double orientationAngle) {
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.positionZ = positionZ;
+		this.orientationX = orientationX;
+		this.orientationY = orientationY;
+		this.orientationZ = orientationZ;
+		this.orientationAngle = orientationAngle;
+	}
+
+	public double getPositionX() {
+		return positionX;
+	}
+
+	public double getPositionY() {
+		return positionY;
+	}
+
+	public double getPositionZ() {
+		return positionZ;
+	}
+
+	public double getOrientationX() {
+		return orientationX;
+	}
+
+	public double getOrientationY() {
+		return orientationY;
+	}
+
+	public double getOrientationZ() {
+		return orientationZ;
+	}
+
+	public double getOrientationAngle() {
+		return orientationAngle;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + positionX + "," + positionY + "," + positionZ + ") ; (" + orientationX + "," + orientationY + ","
+				+ orientationZ + "," + orientationAngle + ")";
+	}
+}
